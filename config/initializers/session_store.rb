0c67b31634519157fd69cf7613bb6efd8829df37
Mike Chau <mchau@iwu.edu>
2013-07-04 08:42:40 -0500

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SaberAlter::Application.config.session_store :cookie_store, key: '_saber-alter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SaberAlter::Application.config.session_store :active_record_store
