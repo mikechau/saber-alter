0f7a8c8af83d7234393fead66593a13beeee52ef
Mike Chau <mchau@iwu.edu>
2013-07-05 13:09:59 -0500

    Add tables for algo and values and update algos form.

--- app/models/algo.rb
+++ app/models/algo.rb
@@ -2,4 +2,6 @@ class Algo < ActiveRecord::Base
   attr_accessible :a, :b, :c, :d, :e, :f, :g, :h, :i, :name, :y_intercept
 
   has_many :values
+  validates_presence_of :a, :b, :c, :d, :e, :f, :g, :h, :i, :name, :y_intercept
+  validates_uniqueness_of :name
 end
