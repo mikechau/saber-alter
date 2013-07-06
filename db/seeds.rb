42461df659e3ecc5345dbf87903bac3939665e69
Mike Chau <mchau@iwu.edu>
2013-07-05 19:15:57 -0500

    Create seeds file for Algo1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Algo.create(
+	   name: 'ALGO1', 
+           y_intercept: 80224, 
+           a: 5228, 
+           b: 7159,
+	   c: 0,
+           d: 1453,
+           e: 2293,
+           f: 440,
+           g: 0,
+           h: 1777,
+           i: 0.03053
+           )
+
+puts "Algo1 created."
